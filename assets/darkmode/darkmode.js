c8ec380c07655b4f115ea9369da0ef4b9fcf6329
ezechukwu nwokedi <israellambert96@gmail.com>
2022-11-07 19:12:09 +0000

    revised toggle, added text

--- assets/darkmode/darkmode.js
+++ assets/darkmode/darkmode.js
@@ -4,6 +4,14 @@ function myFunction() {
         $('.toggle').click(function(){
             $('.toggle').toggleClass('active')
             $('body').toggleClass('night')
+
+            var x = document.getElementById("mode");
+            if (x.innerHTML === "Day") {
+                x.innerHTML = "Night";
+            } else {
+                x.innerHTML = "Day";
+            }
         })
     })
+
 }
